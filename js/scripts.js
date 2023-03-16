99344e4353a67e193938220129c70754040ae199
Ted Zhan <worldcupzhan@hotmail.com>
2023-03-16 15:45:29 -0400

    udpates

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,63 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+// change game mode
+const gameMode = document.getElementById("gameMode");
+const outSelect = document.getElementById("tradeIn1");
+
+gameMode.addEventListener("change", function() {
+    const selectedOption = gameMode.value;
+
+    // clear the current selection
+    outSelect.innerHTML=""
+
+    // Modify options of outSelect based on selectedOption
+    if (selectedOption === "increase") {
+        const option1 = document.createElement("option");
+        option1.value = 0;
+        option1.text = "";
+        outSelect.add(option1);
+
+        const option2 = document.createElement("option");
+        option2.value = "";
+        option2.text = "+50";
+        outSelect.add(option2);
+    } else {
+        const option1 = document.createElement("option");
+        option1.value = 0;
+        option1.text = "";
+        outSelect.add(option1);
+
+        const option2 = document.createElement("option");
+        option2.value = "";
+        option2.text = "Infantry";
+        outSelect.add(option2);
+
+        const option3 = document.createElement("option");
+        option3.value = "";
+        option3.text = "Troops";
+        outSelect.add(option3);
+
+        const option4 = document.createElement("option");
+        option4.value = "";
+        option4.text = "Armies";
+        outSelect.add(option4);
+    }
+});
+
+
+function addToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+    
+function calculate() {
+    var expression = document.getElementById('display').value;
+    var result = eval(expression);
+    document.getElementById('display').value = result;
+}
+    
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
